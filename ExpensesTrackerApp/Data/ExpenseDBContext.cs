f40170901ce323113d7d8f4418eefb7b91d119bc
hafees <hafees.falana@gmail.com>
2022-04-15 18:25:14 -0400

    ExpenseDBContext set up with modifications in startup and appsetting.json

---
+++ ExpensesTrackerApp/Data/ExpenseDBContext.cs
@@ -0,0 +1,18 @@
+﻿using ExpensesTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesTracker.Data
+{
+    public class ExpenseDBContext : DbContext
+    {
+            public ExpenseDBContext(DbContextOptions<ExpenseDBContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<ExpensesTracker.Models.Expense> ExpensesDataBase { get; set; }
+
+    }
+
+    
+}
